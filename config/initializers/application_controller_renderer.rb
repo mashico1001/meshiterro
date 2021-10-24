b54811c0fc2295c2707b430508c4b7f2b072d451
mashico1001 <wabumpr@gmail.com>
2021-10-24 15:08:34 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '626833a186b5ab35760a311a3df25cc6d5a8bd2ee4acaadbffa6a4f07a8bb78ef2dbf414eaa8ab00e4c16e90716fdd0b42e5c624653f1cd10da711ecb4c80f54'
